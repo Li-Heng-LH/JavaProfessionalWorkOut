4bd6e1804206a0cae109021e3e43f2b4f17f04f6
Li Heng <liheng6567@gmail.com>
2021-10-20 14:35:37 +0800

    Starting on primitive Stream

---
+++ functional-programming/src/me/liheng/primitiveStream/Usage.java
@@ -0,0 +1,29 @@
+package me.liheng.primitiveStream;
+
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Usage {
+
+    public static void main(String[] args) {
+
+        //To sum an Integer stream:
+        Stream<Integer> stream = Stream.of(1, 2, 3);
+        System.out.println(
+                stream.reduce(0, (s, n) -> s + n)
+        );
+
+        //To sum an integer stream:
+        Stream<Integer> s = Stream.of(1, 2, 3);
+        System.out.println(
+                s.mapToInt(x -> x)
+                .sum()
+        );
+
+        //Use integer Stream to calculate avg
+        IntStream intStream = IntStream.of(1, 2, 3);
+        OptionalDouble avg = intStream.average();
+        System.out.println(avg.getAsDouble());
+    }
+}
