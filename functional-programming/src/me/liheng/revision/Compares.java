eefa05b09403f35f765f30ec76f6becdb6e7042f
Li Heng <liheng6567@gmail.com>
2021-11-17 10:55:08 +0800

    Revise on Comparator

--- functional-programming/src/me/liheng/revision/Compares.java
+++ functional-programming/src/me/liheng/revision/Compares.java
@@ -1,7 +1,33 @@
 package me.liheng.revision;
 
+import java.util.*;
+
 public class Compares {
 
     public static void main(String[] args) {
+        TreeSet<String> treeSet = new TreeSet<>(Comparator.reverseOrder());
+        treeSet.add("a");
+        treeSet.add("d");
+        treeSet.add("c");
+        treeSet.add("b");
+        System.out.println(treeSet);
+
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("d");
+        list.add("c");
+        list.add("b");
+
+        Collections.sort(list);
+        System.out.println(list);
+
+        Collections.sort(list, (a, b) -> a.compareTo(b));
+        System.out.println(list);
+
+        Collections.sort(list, (a, b) -> b.compareTo(a));
+        System.out.println(list);
+
+        Collections.sort(list, Comparator.reverseOrder());
+        System.out.println(list);
     }
 }
