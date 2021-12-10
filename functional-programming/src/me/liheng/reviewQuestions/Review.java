868a865fb1f44793ee4a0c26ed98edb3d4152d32
Li Heng <liheng6567@gmail.com>
2021-12-10 11:18:59 +0800

    Stream API Review questions

--- functional-programming/src/me/liheng/reviewQuestions/Review.java
+++ functional-programming/src/me/liheng/reviewQuestions/Review.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class Review {
 
     public static void main(String[] args) {
-        question2();
+        preVSpostIncrement();
     }
 
     private static void question1() {
@@ -16,5 +16,27 @@ public class Review {
     private static void question2() {
         Stream<String> stream = Stream.generate(() -> "growl! ");
         stream.limit(3).forEach(System.out::println);
+    }
+    
+    private static void preVSpostIncrement() {
+        // post: the returned value is x itself
+        Stream.iterate(1, x -> x++)
+                .limit(3)
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        // pre: the returned value is the incremented x
+        Stream.iterate(1, x -> ++x)
+                .limit(3)
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        int t = 88;
+        int r = t++;
+        int s = ++t;
+        System.out.println(r); // 88
+        System.out.println(s); // 90
     }
 }
