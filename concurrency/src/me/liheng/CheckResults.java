6ba44a561c643f0ab670cc4300f0f3010bb80e1b
Li Heng <liheng6567@gmail.com>
2022-01-24 18:02:31 +0800

    Concurrency: threads accessing static variable

--- concurrency/src/me/liheng/CheckResults.java
+++ concurrency/src/me/liheng/CheckResults.java
@@ -7,12 +7,12 @@ public class CheckResults {
     public static void main(String[] args) throws InterruptedException {
 
         new Thread(() -> {
-            for (int i = 0; i< 500000; i++) counter++;
+            for (int i = 0; i< 500000; i++) counter++; //Note the separate thread can access class variable
         }).start();
 
         while (counter < 200000) {
             System.out.println("Not reached yet");
-            Thread.sleep(1000); // 1 SECOND
+            Thread.sleep(1000); // Current thread to sleep for 1 SECOND
         }
 
         System.out.println("Reached");
