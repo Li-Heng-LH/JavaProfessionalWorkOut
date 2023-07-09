49ace487ef9c682a5a1a925b4efca3c4f5b09bd3
Li Heng <liheng6567@gmail.com>
2023-07-09 23:21:59 +0800

    ambiguous lambda expression

---
+++ concurrency/src/me/liheng/AmbiguousLambda.java
@@ -0,0 +1,16 @@
+package me.liheng;
+
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
+public class AmbiguousLambda {
+
+    public static void main(String[] args) {
+        // use(() -> 9); //Ambiguous method call.
+        // use(() -> {throw new Exception();}); //Ambiguous method call.
+        use((Callable<Integer>) () -> 9);  //explicit cast
+    }
+
+    public static void use(Supplier<Integer> supplier) {}
+    public static void use(Callable<Integer> callable) {}
+}
