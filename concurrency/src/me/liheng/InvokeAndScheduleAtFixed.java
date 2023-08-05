c1ebd77886cfed75fc34123feda8ca42c959f008
Li Heng <liheng6567@gmail.com>
2023-08-05 21:59:30 +0800

    future.get is blocking

--- concurrency/src/me/liheng/InvokeAndScheduleAtFixed.java
+++ concurrency/src/me/liheng/InvokeAndScheduleAtFixed.java
@@ -2,17 +2,14 @@ package me.liheng;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class InvokeAndScheduleAtFixed {
-    private static ExecutorService service = null;
+    private static ScheduledExecutorService service = null;
 
     public static void main(String[] args) {
         try {
-            invoke();
+            submit();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -21,7 +18,7 @@ public class InvokeAndScheduleAtFixed {
     }
 
     static void invoke() throws Exception {
-        service = Executors.newSingleThreadExecutor();
+        service = Executors.newSingleThreadScheduledExecutor();
         List<Callable<Integer>> futures = Arrays.asList(
                 () -> {System.out.println(Thread.currentThread()); Thread.sleep(2_000); return 1;},
                 () -> {System.out.println(Thread.currentThread()); Thread.sleep(2_000); return 2;},
@@ -34,7 +31,24 @@ public class InvokeAndScheduleAtFixed {
         }
     }
 
-    static void scheduleAtFixed() {
-        //
+    static void scheduleAtFixed() throws Exception {
+        service = Executors.newSingleThreadScheduledExecutor();
+        ScheduledFuture<?> future = service.scheduleAtFixedRate(
+                () -> System.out.println(Thread.currentThread()),
+                0,
+                1,
+                TimeUnit.SECONDS
+        );
+        System.out.println("future.get(): " + future.get());  //blocking(waiting)
+    }
+
+    static void submit() throws Exception {
+        service = Executors.newSingleThreadScheduledExecutor();
+        Future<Integer> future = service.submit(() -> {
+            System.out.println(Thread.currentThread());
+            Thread.sleep(3_000);
+            return 2;
+        });
+        System.out.println("future.get(): " + future.get()); //blocking(waiting)
     }
 }
---
+++ concurrency/src/me/liheng/ThreadPool.java
@@ -0,0 +1,4 @@
+package me.liheng;
+
+public class ThreadPool {
+}
