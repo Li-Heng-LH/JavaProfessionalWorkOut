21b5ecff65204209ee745db3add6c82e83a199b6
Li Heng <liheng6567@gmail.com>
2023-08-07 00:23:14 +0800

    Problem with newScheduledThreadPool

---
+++ concurrency/src/me/liheng/BlockingNonBlocking.java
@@ -0,0 +1,28 @@
+package me.liheng;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class BlockingNonBlocking {
+
+    private static ScheduledExecutorService service = null;
+
+    public static void main(String[] args) {
+        try {
+            test();
+            System.out.println("Main");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (service != null) service.shutdown();
+        }
+    }
+
+    static void test() throws Exception {
+        service = Executors.newScheduledThreadPool(3);
+        service.scheduleAtFixedRate(() -> {
+            System.out.println("thread");
+        }, 0, 2, TimeUnit.SECONDS);
+    }
+}
---
+++ concurrency/src/me/liheng/ChoosingPoolSize.java
@@ -0,0 +1,12 @@
+package me.liheng;
+
+public class ChoosingPoolSize {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+            Runtime.getRuntime().availableProcessors()
+        );
+
+    }
+}
