4650aea7723af6ed147020c7c0ed2eb89ad7d975
Li Heng <liheng6567@gmail.com>
2024-01-28 23:57:10 +0800

    Using Future: get with timeout

--- concurrency/src/me/liheng/UsingFuture.java
+++ concurrency/src/me/liheng/UsingFuture.java
@@ -3,10 +3,15 @@ package me.liheng;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class UsingFuture {
 
     public static void main(String[] args) {
+        getWithTimeOut();
+    }
+
+    public static void futureDotGet() {
         ExecutorService service = null;
         try {
             service = Executors.newSingleThreadExecutor();
@@ -22,6 +27,23 @@ public class UsingFuture {
             if (service != null) {
                 service.shutdown();
             }
+        }
+    }
+
+    public static void getWithTimeOut() {
+        ExecutorService service = null;
+        try {
+            service = Executors.newSingleThreadExecutor();
+            Future<Integer> future = service.submit(() -> {
+                System.out.println(Thread.currentThread().getName() + " Waiting...");
+                Thread.sleep(5_000);
+                return 1;
+            });
+            System.out.println(future.get(2, TimeUnit.SECONDS));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (service != null) service.shutdown();
         }
     }
 }
